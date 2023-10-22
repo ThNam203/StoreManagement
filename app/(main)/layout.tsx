c7763714ffb6498a6f6cf05496ec6333707fc0d5
ThNam203 <hthnam203@gmail.com>
2023-10-22 16:18:49 +0700

    make sidebar closable

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,8 +1,11 @@
+"use client";
 import "../globals.css";
 import type { Metadata } from "next";
 import { Open_Sans } from "next/font/google";
 import SideBar from "@/components/ui/overview/overview_sidebar";
 import no_scrollbar_style from "../../styles/no_scrollbar.module.css";
+import { useCallback, useEffect, useState } from "react";
+import { cn } from "@/lib/utils";
 const font = Open_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -15,11 +18,48 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const [isSideBarCollapsed, setIsSideBarCollapsed] = useState<boolean | null>(
+    null
+  );
+  const [isWindowLarge, setIsWindowLarge] = useState(true);
+
+  useEffect(() => {
+    if (window.innerWidth < 1024) setIsWindowLarge(false);
+    else setIsWindowLarge(true);
+
+    const screenObserver = (e: MediaQueryListEvent) => {
+      setIsWindowLarge(!e.matches);
+    };
+
+    const mql = window.matchMedia("(max-width: 1023px)");
+    mql.addEventListener("change", screenObserver);
+
+    return () => {
+      mql.removeEventListener("change", screenObserver);
+    };
+  }, []);
+
+  const changeSideBarCollapsibilityOnClick = () => {
+    if (isSideBarCollapsed == null) {
+      if (isWindowLarge) setIsSideBarCollapsed(true);
+      else setIsSideBarCollapsed(false);
+    } else setIsSideBarCollapsed((prev) => !prev);
+  };
+
   return (
     <html lang="en">
       <body className={font.className + "  bg-slate-100"}>
-        <SideBar className={no_scrollbar_style["no-scrollbar"] + " z-10 bg-white"} />
-        <div className="bg-slate-100 ml-[80px] lg:ml-[216px] py-4 pr-4">
+        <SideBar
+          isSideBarCollapsed={isSideBarCollapsed}
+          changeSideBarCollapsibility={changeSideBarCollapsibilityOnClick}
+          className={no_scrollbar_style["no-scrollbar"] + " z-10 bg-white shadow-gray-300 shadow-md"}
+        />
+        <div
+          className={cn(
+            "bg-slate-100 ml-[80px] py-4 pr-4 ease-linear duration-150",
+            isSideBarCollapsed ? "" : "lg:ml-[216px]"
+          )}
+        >
           {children}
         </div>
       </body>
--- components/ui/overview/overview_sidebar.tsx
+++ components/ui/overview/overview_sidebar.tsx
@@ -1,3 +1,4 @@
+"use client";
 import {
   GanttChartSquare,
   BarChart3,
@@ -19,8 +20,8 @@ import {
   Users,
   User2,
   Truck,
+  ArrowLeftCircle,
 } from "lucide-react";
-import { Separator } from "../separator";
 import Image from "next/image";
 import { cn } from "@/lib/utils";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
@@ -31,87 +32,86 @@ import {
   AccordionTrigger,
 } from "../accordion";
 import { Button } from "../button";
+import React, { useState } from "react";
+
+const LucideIcons = (iconName: string, isCollapsed: boolean | null) => {
+  const iconSize = 16;
+  const iconClasses = cn(
+    "m-auto",
+    isCollapsed == null ? "lg:ml-4 lg:mr-4" : "",
+    isCollapsed == true ? "" : isCollapsed == false ? "ml-4 mr-4" : ""
+  );
+  let icon: JSX.Element;
+  switch (iconName) {
+    case "GanttChartSquare":
+      return <GanttChartSquare size={iconSize} className={iconClasses} />;
+    case "Receipt":
+      return <Receipt size={iconSize} className={iconClasses} />;
+    case "Users":
+      return <Users size={iconSize} className={iconClasses} />;
+    case "BarChart3":
+      return <BarChart3 size={iconSize} className={iconClasses} />;
+    case "ShoppingCart":
+      return <ShoppingCart size={iconSize} className={iconClasses} />;
+    case "ShoppingBag":
+      return <ShoppingBag size={iconSize} className={iconClasses} />;
+    case "Package":
+      return <Package size={iconSize} className={iconClasses} />;
+    case "User":
+      return <User size={iconSize} className={iconClasses} />;
+    case "Contact":
+      return <Contact size={iconSize} className={iconClasses} />;
+    case "CalendarClock":
+      return <CalendarClock size={iconSize} className={iconClasses} />;
+    case "Settings":
+      return <Settings size={iconSize} className={iconClasses} />;
+    case "LogOut":
+      return <LogOut size={iconSize} className={iconClasses} />;
+    case "UserCog":
+      return <UserCog size={iconSize} className={iconClasses} />;
+    case "Wrench":
+      return <Wrench size={iconSize} className={iconClasses} />;
+    case "MenuSquare":
+      return <MenuSquare size={iconSize} className={iconClasses} />;
+    case "CalendarRange":
+      return <CalendarRange size={iconSize} className={iconClasses} />;
+    case "Boxes":
+      return <Boxes size={iconSize} className={iconClasses} />;
+    case "Group":
+      return <Group size={iconSize} className={iconClasses} />;
+    case "User2":
+      return <User2 size={iconSize} className={iconClasses} />;
+    case "Truck":
+      return <Truck size={iconSize} className={iconClasses} />;
+  }
+};
 
 const SideBarButton = (
   iconName: string,
   title: string,
-  className: string = ""
+  className: string = "",
+  isCollapsed: boolean | null
 ) => {
-  const iconSize = 16;
-  const iconClasses =
-    "m-auto lg:ml-4 lg:mr-4 group-hover:ml-4 group-hover:mr-4";
-  let icon: JSX.Element;
-  switch (iconName) {
-    case "GanttChartSquare":
-      icon = <GanttChartSquare size={iconSize} className={iconClasses} />;
-      break;
-    case "BarChart3":
-      icon = <BarChart3 size={iconSize} className={iconClasses} />;
-      break;
-    case "ShoppingCart":
-      icon = <ShoppingCart size={iconSize} className={iconClasses} />;
-      break;
-    case "ShoppingBag":
-      icon = <ShoppingBag size={iconSize} className={iconClasses} />;
-      break;
-    case "Package":
-      icon = <Package size={iconSize} className={iconClasses} />;
-      break;
-    case "User":
-      icon = <User size={iconSize} className={iconClasses} />;
-      break;
-    case "Contact":
-      icon = <Contact size={iconSize} className={iconClasses} />;
-      break;
-    case "CalendarClock":
-      icon = <CalendarClock size={iconSize} className={iconClasses} />;
-      break;
-    case "Settings":
-      icon = <Settings size={iconSize} className={iconClasses} />;
-      break;
-    case "LogOut":
-      icon = <LogOut size={iconSize} className={iconClasses} />;
-      break;
-    case "UserCog":
-      icon = <UserCog size={iconSize} className={iconClasses} />;
-      break;
-    case "Wrench":
-      icon = <Wrench size={iconSize} className={iconClasses} />;
-      break;
-    case "MenuSquare":
-      icon = <MenuSquare size={iconSize} className={iconClasses} />;
-      break;
-    case "CalendarRange":
-      icon = <CalendarRange size={iconSize} className={iconClasses} />;
-      break;
-    case "Boxes":
-      icon = <Boxes size={iconSize} className={iconClasses} />;
-      break;
-    case "Group":
-      icon = <Group size={iconSize} className={iconClasses} />;
-      break;
-    case "User2":
-      icon = <User2 size={iconSize} className={iconClasses} />;
-      break;
-    case "Truck":
-      icon = <Truck size={iconSize} className={iconClasses} />;
-      break;
-  }
+  const icon = LucideIcons(iconName, isCollapsed);
 
   return (
     <div
-      className={
-        cn(
-          "hover:text-blue-700 hover:bg-blue-100",
-          title == "Logout" && "hover:text-red-700 hover:bg-red-200"
-        ) +
+      className={cn(
+        "hover:text-blue-700 hover:bg-blue-100",
+        title == "Logout" && "hover:text-red-700 hover:bg-red-200",
         " w-5/6 flex justify-start items-center min-h-[2.5rem] h-10 my-1" +
-        " rounded-sm hover:cursor-pointer " +
+          " rounded-sm hover:cursor-pointer ",
         className
-      }
+      )}
     >
       {icon!}
-      <p className="text-sm hidden lg:block group-hover:block">
+      <p
+        className={cn(
+          "text-sm",
+          isCollapsed == null ? "hidden lg:block" : "",
+          isCollapsed == true ? "hidden" : isCollapsed == false ? "block" : ""
+        )}
+      >
         {title}
         {
           /* "ORDERS section needs an aditional which tells how much orders is being processed" */
@@ -127,163 +127,220 @@ const SideBarButton = (
   );
 };
 
-const SideBar = ({ className }: { className: string }) => {
+const SideBarAccordion = (
+  iconName: string,
+  title: string,
+  buttons: JSX.Element[],
+  isCollapsed: boolean | null
+) => {
   return (
-    <div
-      className={cn(
-        "flex flex-col fixed left-0 h-full overflow-y-scroll items-center min-w-[64px] w-[64px] lg:min-w-[200px] lg:flex-[0.2] group hover:w-[200px] hover:min-w-[200px] ease-linear duration-200",
-        className
-      )}
+    <Accordion
+      type="single"
+      collapsible={true}
+      className="w-5/6 bg-slate-100 rounded-xl mx-2 my-1 hover:cursor-pointer"
     >
-      <div className="flex flex-row items-center mx-auto">
-        {/* <img src="/static/web_avatar.png" alt="web avatar" className="w-[32px] h-[32px]"/> */}
-        <Image src="/web_avatar.png" alt="web avatar" width={48} height={48} />
-        <h1 className="ml-2 hidden font-bold text-xl tracking-tight lg:block group-hover:block">
-          MSTORE
-        </h1>
+      <AccordionItem value="item-1">
+        <AccordionTrigger
+          showArrowFunc={cn(
+            "mr-2",
+            isCollapsed == null ? "hidden lg:block" : "",
+            isCollapsed == true ? "hidden" : isCollapsed == false ? "block" : ""
+          )}
+          className={cn(
+            "justify-center gap-2 h-12",
+            isCollapsed == null ? "justify-center lg:justify-start" : "",
+            isCollapsed == true
+              ? "justify-center"
+              : isCollapsed == false
+              ? "justify-start"
+              : "",
+          )}
+        >
+          <div className="flex flex-row w-full">
+            {LucideIcons(iconName, isCollapsed)}
+            <p
+              className={cn(
+                "text-sm font-normal",
+                isCollapsed == null ? "hidden lg:block" : "",
+                isCollapsed == true
+                  ? "hidden"
+                  : isCollapsed == false
+                  ? "block"
+                  : ""
+              )}
+            >
+              {title}
+            </p>
+          </div>
+        </AccordionTrigger>
+        <AccordionContent className="mx-2">{...buttons}</AccordionContent>
+      </AccordionItem>
+    </Accordion>
+  );
+};
+
+const SideBar = ({
+  className,
+  changeSideBarCollapsibility,
+  isSideBarCollapsed: isCollapsed,
+}: {
+  className: string;
+  changeSideBarCollapsibility: () => void;
+  isSideBarCollapsed: boolean | null;
+}) => {
+  return (
+    <div>
+      <ArrowLeftCircle
+        height={20}
+        width={20}
+        color="#FFFFFF"
+        fill="rgb(96 165 250)"
+        onClick={() => changeSideBarCollapsibility()}
+        className={cn(
+          "fixed rounded-full translate-x-[-50%] z-20 top-8 ease-linear duration-150 hover:cursor-pointer",
+          isCollapsed == null
+            ? "max-lg:rotate-180 left-[64px] lg:left-[200px]"
+            : "",
+          isCollapsed == true
+            ? "left-[64px] rotate-180"
+            : isCollapsed == false
+            ? "left-[200px]"
+            : ""
+        )}
+      />
+      <div
+        className={cn(
+          "flex flex-col fixed left-0 top-0 h-full overflow-y-scroll items-center ease-linear duration-200",
+          className,
+          isCollapsed == null ? "w-[64px] lg:w-[200px]" : "",
+          isCollapsed == true
+            ? "w-[64px]"
+            : isCollapsed == false
+            ? "w-[200px]"
+            : ""
+        )}
+      >
+        <div className="flex flex-row w-full items-center justify-center relative overflow-y-visible">
+          {/* <img src="/static/web_avatar.png" alt="web avatar" className="w-[32px] h-[32px]"/> */}
+          <Image
+            src="/web_avatar.png"
+            alt="web avatar"
+            width={48}
+            height={48}
+          />
+          <h1
+            className={cn(
+              "ml-2 font-bold text-xl tracking-tight",
+              isCollapsed == null ? "hidden lg:block" : "",
+              isCollapsed == true
+                ? "hidden"
+                : isCollapsed == false
+                ? "block"
+                : ""
+            )}
+          >
+            MSTORE
+          </h1>
+        </div>
+        <Accordion
+          type="single"
+          collapsible={true}
+          className="w-5/6 bg-slate-100 rounded-xl mx-2 my-2 px-2 hover:cursor-pointer"
+        >
+          <AccordionItem value="item-1">
+            <AccordionTrigger
+              showArrowFunc={cn(
+                isCollapsed == null ? "hidden lg:block" : "",
+                isCollapsed == true
+                  ? "hidden"
+                  : isCollapsed == false
+                  ? "block"
+                  : ""
+              )}
+              className="gap-2"
+            >
+              <Avatar className="w-9 h-9 border-2 border-dashed border-blue-500 hover:border-solid">
+                <AvatarImage src="https://github.com/shadcn.png" />
+                <AvatarFallback>CN</AvatarFallback>
+              </Avatar>
+              <div
+                className={cn(
+                  isCollapsed == null ? "hidden lg:block" : "",
+                  isCollapsed == true
+                    ? "hidden"
+                    : isCollapsed == false
+                    ? "block"
+                    : ""
+                )}
+              >
+                <h4 className="font-medium text-sm max-w-[85px] text-start break-words">
+                  Nam Huynh
+                </h4>
+                <p className="text-xs opacity-75 text-start flex-wrap">Staff</p>
+              </div>
+            </AccordionTrigger>
+            <AccordionContent>
+              {SideBarButton("UserCog", "Edit profile", "w-full", isCollapsed)}
+              {SideBarButton("LogOut", "Logout", "w-full", isCollapsed)}
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
+
+        {SideBarAccordion(
+          "Receipt",
+          "Sales",
+          [SideBarButton("ShoppingCart", "Orders", "!w-full", isCollapsed)],
+          isCollapsed
+        )}
+
+        {SideBarAccordion(
+          "Users",
+          "Partners",
+          [
+            SideBarButton("User2", "Customer group", "!w-full", isCollapsed),
+            SideBarButton("User", "Customers", "!w-full", isCollapsed),
+            SideBarButton("Truck", "Supplier", "!w-full", isCollapsed),
+          ],
+          isCollapsed
+        )}
+
+        {SideBarButton("Package", "Warehouse", "", isCollapsed)}
+        {SideBarButton("BarChart3", "Report", "", isCollapsed)}
+
+        {SideBarAccordion(
+          "ShoppingBag",
+          "Products",
+          [
+            SideBarButton("Group", "Product group", "!w-full", isCollapsed),
+            SideBarButton("Boxes", "Products", "!w-full", isCollapsed),
+          ],
+          isCollapsed
+        )}
+
+        {SideBarAccordion(
+          "Contact",
+          "Staff",
+          [
+            SideBarButton("UserCog", "Staff group", "!w-full", isCollapsed),
+            SideBarButton(
+              "MenuSquare",
+              "Staff account",
+              "!w-full",
+              isCollapsed
+            ),
+            SideBarButton("Wrench", "Role setting", "!w-full", isCollapsed),
+            SideBarButton(
+              "CalendarRange",
+              "Work manage",
+              "!w-full",
+              isCollapsed
+            ),
+          ],
+          isCollapsed
+        )}
+
+        {SideBarButton("Settings", "Settings", "", isCollapsed)}
       </div>
-      <Accordion
-        type="single"
-        collapsible={true}
-        className="w-5/6 bg-slate-100 rounded-xl mx-2 my-2 px-2 hover:cursor-pointer"
-      >
-        <AccordionItem value="item-1">
-          <AccordionTrigger
-            showArrowFunc="lg:block hidden group-hover:block"
-            className="gap-2"
-          >
-            <Avatar className="w-9 h-9 border-2 border-dashed border-blue-500 hover:border-solid">
-              <AvatarImage src="https://github.com/shadcn.png" />
-              <AvatarFallback>CN</AvatarFallback>
-            </Avatar>
-            <div className="hidden group-hover:block lg:block">
-              <h4 className="font-medium text-sm max-w-[85px] text-start break-words">
-                Nam Huynh
-              </h4>
-              <p className="text-xs opacity-75 text-start flex-wrap">Staff</p>
-            </div>
-          </AccordionTrigger>
-          <AccordionContent>
-            {SideBarButton("UserCog", "Edit profile", "w-full")}
-            {SideBarButton("LogOut", "Logout", "w-full")}
-          </AccordionContent>
-        </AccordionItem>
-      </Accordion>
-
-      <Accordion
-        type="single"
-        collapsible={true}
-        className="w-5/6 bg-slate-100 rounded-xl mx-2 my-1 lg:pr-2 group-hover:pr-2 hover:cursor-pointer"
-      >
-        <AccordionItem value="item-1">
-          <AccordionTrigger
-            showArrowFunc="lg:block hidden group-hover:block"
-            className="justify-center lg:justify-start group-hover:justify-start gap-2 h-12"
-          >
-            <div className="flex flex-row w-full">
-              <Receipt
-                size={16}
-                className="m-auto lg:ml-4 lg:mr-4 group-hover:ml-4 group-hover:mr-4"
-              />
-              <p className="text-sm font-normal hidden lg:block group-hover:block">
-                Sales
-              </p>
-            </div>
-          </AccordionTrigger>
-          <AccordionContent className="ml-2">
-            {SideBarButton("ShoppingCart", "Orders", "w-full")}
-          </AccordionContent>
-        </AccordionItem>
-      </Accordion>
-
-      <Accordion
-        type="single"
-        collapsible={true}
-        className="w-5/6 bg-slate-100 rounded-xl mx-2 my-1 lg:pr-2 group-hover:pr-2  hover:cursor-pointer"
-      >
-        <AccordionItem value="item-1">
-          <AccordionTrigger
-            showArrowFunc="lg:block hidden group-hover:block"
-            className="justify-center lg:justify-start group-hover:justify-start gap-2 h-12"
-          >
-            <div className="flex flex-row w-full">
-              <Users
-                size={16}
-                className="m-auto lg:ml-4 lg:mr-4 group-hover:ml-4 group-hover:mr-4"
-              />
-              <p className="text-sm font-normal hidden lg:block group-hover:block">
-                Partners
-              </p>
-            </div>
-          </AccordionTrigger>
-          <AccordionContent className="ml-2">
-            {SideBarButton("User2", "Customer group", "w-full")}
-            {SideBarButton("User", "Customers", "w-full")}
-            {SideBarButton("Truck", "Supplier", "w-full")}
-          </AccordionContent>
-        </AccordionItem>
-      </Accordion>
-
-      {SideBarButton("Package", "Warehouse")}
-      {SideBarButton("BarChart3", "Report")}
-
-      <Accordion
-        type="single"
-        collapsible={true}
-        className="w-5/6 bg-slate-100 rounded-xl mx-2 my-1 lg:pr-2 group-hover:pr-2 hover:cursor-pointer"
-      >
-        <AccordionItem value="item-1">
-          <AccordionTrigger
-            showArrowFunc="lg:block hidden group-hover:block"
-            className="justify-center lg:justify-start group-hover:justify-start gap-2 h-12"
-          >
-            <div className="flex flex-row w-full">
-              <ShoppingBag
-                size={16}
-                className="m-auto lg:ml-4 lg:mr-4 group-hover:ml-4 group-hover:mr-4"
-              />
-              <p className="text-sm font-normal hidden lg:block group-hover:block">
-                Products
-              </p>
-            </div>
-          </AccordionTrigger>
-          <AccordionContent className="ml-2">
-            {SideBarButton("Group", "Product group", "w-full pl-0")}
-            {SideBarButton("Boxes", "Products", "w-full")}
-          </AccordionContent>
-        </AccordionItem>
-      </Accordion>
-
-      <Accordion
-        type="single"
-        collapsible={true}
-        className="w-5/6 bg-slate-100 rounded-xl mx-2 my-1 lg:pr-2 group-hover:pr-2 hover:cursor-pointer"
-      >
-        <AccordionItem value="item-1">
-          <AccordionTrigger
-            showArrowFunc="lg:block hidden group-hover:block"
-            className="justify-center lg:justify-start group-hover:justify-start gap-2 h-12"
-          >
-            <div className="flex flex-row w-full">
-              <Contact
-                size={16}
-                className="m-auto lg:ml-4 lg:mr-4 group-hover:ml-4 group-hover:mr-4"
-              />
-              <p className="text-sm font-normal hidden lg:block group-hover:block">
-                Staff
-              </p>
-            </div>
-          </AccordionTrigger>
-          <AccordionContent className="ml-2">
-            {SideBarButton("UserCog", "Staff group", "w-full")}
-            {SideBarButton("MenuSquare", "Staff account", "w-full")}
-            {SideBarButton("Wrench", "Role setting", "w-full")}
-            {SideBarButton("CalendarRange", "Work manage", "w-full")}
-          </AccordionContent>
-        </AccordionItem>
-      </Accordion>
-
-      {SideBarButton("Settings", "Settings")}
     </div>
   );
 };
