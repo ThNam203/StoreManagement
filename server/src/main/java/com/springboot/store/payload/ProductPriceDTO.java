a004b2daabfdc39821724641c35c796b1e556689
Khooiiiii <21521010@gm.uit.edu.vn>
2023-10-30 14:13:18 +0700

    doing something good :vvv

---
+++ server/src/main/java/com/springboot/store/payload/ProductPriceDTO.java
@@ -0,0 +1,10 @@
+package com.springboot.store.payload;
+
+public class ProductPriceDTO {
+    private int id;
+    private double price;
+    private String name;
+    private String note;
+    private SalesUnitsDTO unit;
+    private ProductDTO product;
+}
