5be2da1d5c6d097651ae8cf00dfaa36813ffb0b8
Khooiiiii <21521010@gm.uit.edu.vn>
2023-11-26 21:59:35 +0700

    helo

--- server/src/main/java/com/springboot/store/service/impl/StaffServiceImpl.java
+++ server/src/main/java/com/springboot/store/service/impl/StaffServiceImpl.java
@@ -199,6 +199,7 @@ public class StaffServiceImpl implements StaffService {
                 .phoneNumber(staff.getPhoneNumber())
                 .position(staff.getPosition())
                 .salaryDebt(staff.getSalaryDebt())
+                .sex(staff.getSex())
                 .address(staff.getAddress())
                 .note(staff.getNote())
                 .birthday(staff.getBirthday())
