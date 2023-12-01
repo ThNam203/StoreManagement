c060842091d8cb1007d934d73fad4bb8d73440e7
ptdat4823 <phamtiendat4823@gmail.com>
2023-12-01 19:50:39 +0700

    add delete staff

--- services/staff_service.ts
+++ services/staff_service.ts
@@ -5,14 +5,23 @@ const getAllStaffs = () => {
   return AxiosService.get<Staff[]>("/api/staffs");
 };
 const createNewStaff = (data: any) => {
-  console.log("data to sent", data);
   return AxiosService.post<Staff>("/api/staffs", data, {
     headers: { "Content-Type": "multipart/form-data" },
   });
 };
+const updateStaff = (data: any) => {
+  return AxiosService.post<Staff>("/api/staffs/" + data.id, data, {
+    headers: { "Content-Type": "multipart/form-data" },
+  });
+};
+const deleteStaff = (id: any) => {
+  return AxiosService.delete("/api/staffs/" + id);
+};
 const StaffService = {
   getAllStaffs,
   createNewStaff,
+  updateStaff,
+  deleteStaff,
 };
 
 export default StaffService;
