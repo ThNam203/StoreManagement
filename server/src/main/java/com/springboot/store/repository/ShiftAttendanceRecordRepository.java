19545039c1895d74665c63f0a8d1d448055dba50
Khooiiiii <21521010@gm.uit.edu.vn>
2023-12-12 19:26:42 +0700

    delete shift attendance record when delete staff

--- server/src/main/java/com/springboot/store/repository/ShiftAttendanceRecordRepository.java
+++ server/src/main/java/com/springboot/store/repository/ShiftAttendanceRecordRepository.java
@@ -10,4 +10,6 @@ import java.util.List;
 public interface ShiftAttendanceRecordRepository extends JpaRepository<ShiftAttendanceRecord, Integer> {
     @Query("SELECT s FROM ShiftAttendanceRecord s WHERE s.staffId = :staffId AND YEAR(s.date) = YEAR(CURRENT_DATE) AND MONTH(s.date) = MONTH(CURRENT_DATE)")
     List<ShiftAttendanceRecord> findByStaffIdAndDateInThisMonth(@Param("staffId") int staffId);
+
+    List<ShiftAttendanceRecord> findByStaffId(int staffId);
 }
